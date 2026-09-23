5a5aa5722f326e0d298f6811319c3150962abe75
RealCitizenSnips <causeofjunk1211@yahoo.com>
2026-09-23 17:01:18 -0500

    v0.10.0 bump service worker cache and include icons/placeholder.svg

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "nioh3-builder-v0.9.9";
-const ASSETS = ["./","index.html","manifest.json","title-banner.png","icon.svg"];
+const CACHE = "nioh3-builder-v0.10.0";
+const ASSETS = ["./","index.html","manifest.json","title-banner.png","icon.svg","icons/placeholder.svg"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
